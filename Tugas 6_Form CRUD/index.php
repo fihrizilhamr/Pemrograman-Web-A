caedd6b8f0cc6b3a23161c5cc71fb52d93251dac
fihrizilhamr <116176265+fihrizilhamr@users.noreply.github.com>
2023-10-31 22:20:39 +0700

    Tugas 6

---
+++ Tugas 6_Form CRUD/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Pendaftaran Siswa Baru | Pemrograman Web A</title>
+</head>
+
+<body>
+	<header>
+		<h3>Pendaftaran Siswa Baru</h3>
+		<h1>Pemrograman Web A</h1>
+	</header>
+	
+	<h4>Menu</h4>
+	<nav>
+		<ul>
+			<li><a href="form-daftar.php">Daftar Baru</a></li>
+			<li><a href="list-siswa.php">Pendaftar</a></li>
+		</ul>
+	</nav>
+	
+	
+	<?php if(isset($_GET['status'])): ?>
+	<p>
+		<?php
+			if($_GET['status'] == 'sukses'){
+				echo "Pendaftaran siswa baru berhasil!";
+			} else {
+				echo "Pendaftaran gagal!";
+			}
+		?>
+	</p>
+	<?php endif; ?>
+	
+	</body>
+</html>
