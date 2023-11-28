7cd196b63b3e9ad3b608ee35c5e45d6014d7fefc
fihrizilhamr <116176265+fihrizilhamr@users.noreply.github.com>
2023-11-28 09:28:16 +0700

    latihan 11

---
+++ Latihan_11/create.php
@@ -0,0 +1,34 @@
+<html>
+<head>
+	<title>Create Data</title>
+</head>
+
+<body>
+	<h2>Create Data</h2>
+	<p>
+		<a href="index.php">Home</a>
+	</p>
+
+	<form action="create_action.php" method="post" name="add">
+		<table width="25%" border="0">
+			<tr> 
+				<td>Name</td>
+				<td><input type="text" name="name"></td>
+			</tr>
+			<tr> 
+				<td>Age</td>
+				<td><input type="text" name="age"></td>
+			</tr>
+			<tr> 
+				<td>Email</td>
+				<td><input type="text" name="email"></td>
+			</tr>
+			<tr> 
+				<td></td>
+				<td><input type="submit" name="submit" value="Add"></td>
+			</tr>
+		</table>
+	</form>
+</body>
+</html>
+
